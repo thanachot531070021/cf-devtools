6293ba673979f730bf1df055af0cd19fc73e3723
Neung-Thanakrit <Thanakrit_C@THANAKRIT>
2026-02-17 22:47:40 +0700

    sample api

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,35 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+	const url = new URL(request.url);
+    const method = request.method;
+
+// GET /
+    if (url.pathname === "/" && method === "GET") {
+      return Response.json({ message: "Hello Worker API 🚀" });
+    }
+
+    // GET /api/hello
+    if (url.pathname === "/api/hello" && method === "GET") {
+      const name = url.searchParams.get("name") || "Guest";
+      return Response.json({ greeting: `Hello ${name}` });
+    }
+
+    // POST /api/echo
+    if (url.pathname === "/api/echo" && method === "POST") {
+      const body = await request.json();
+      return Response.json({
+        you_sent: body,
+      });
+    }
+
+    // GET /api/time
+    if (url.pathname === "/api/time" && method === "GET") {
+      return Response.json({
+        now: new Date().toISOString(),
+      });
+    }
+
 		return new Response("Hello Worker!");
 	},
 } satisfies ExportedHandler<Env>;
